95837eae1ad7bbaca3a5e9acc1458cef52f712cd
JUNHA CHOI <choijh0309@naver.com>
2024-04-10 18:00:01 +0900

    service layer commit

---
+++ src/main/java/com/codestates/member/MemberController.java
@@ -0,0 +1,82 @@
+package com.codestates.member;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Positive;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@RestController
+@RequestMapping("/v5/members")
+@Validated
+public class MemberController {
+    private final MemberService memberService;
+    private final MemberMapper mapper;
+
+    public MemberController(MemberService memberService, MemberMapper mapper) {
+        this.memberService = memberService;
+        this.mapper = mapper;
+    }
+
+    @PostMapping
+    public ResponseEntity postMember(@Valid @RequestBody MemberPostDto memberDto) {
+        Member member = mapper.memberPostDtoToMember(memberDto);
+
+        Member response = memberService.createMember(member);
+
+        return new ResponseEntity<>(mapper.memberToMemberResponseDto(response),
+                HttpStatus.CREATED);
+    }
+
+    @PatchMapping("/{member-id}")
+    public ResponseEntity patchMember(@PathVariable("member-id") @Positive long memberId,
+                                      @Valid @RequestBody MemberPatchDto memberPatchDto) {
+        memberPatchDto.setMemberId(memberId);
+
+        Member response = memberService.updateMember(mapper.memberPatchDtoToMember(memberPatchDto));
+
+        // No need Business logic
+
+        return new ResponseEntity<>(mapper.memberToMemberResponseDto(response),
+                HttpStatus.OK);
+    }
+
+    @GetMapping("/{member-id}")
+    public ResponseEntity getMember(@PathVariable("member-id") @Positive long memberId) {
+        Member response = memberService.findMember(memberId);
+
+        // not implementation
+        return new ResponseEntity<>(mapper.memberToMemberResponseDto(response),
+                HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity getMembers() {
+        List<Member> members = memberService.findMembers();
+
+        List<MemberResponseDto> response =
+                members.stream()
+                        .map(member -> mapper.memberToMemberResponseDto(member))
+                        .collect(Collectors.toList());
+        // not implementation
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{member-id}")
+    public ResponseEntity deleteMember(@PathVariable("member-id") @Positive long memberId) {
+        System.out.println("# deleted memberId: " + memberId);
+
+        memberService.deleteMember(memberId);
+        // No need business logic
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    // 회원 정보는 구현해야 할 실습이 없습니다!
+}
